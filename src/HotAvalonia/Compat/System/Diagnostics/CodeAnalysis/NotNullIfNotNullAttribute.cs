3cf02bc2ad338561681a4c517a8171399754c40a
Kir_Antipov <kp.antipov@gmail.com>
2025-02-15 21:31:56 +0300

    Use `Nullable` instead of reinventing the wheel

--- src/HotAvalonia/Compat/System/Diagnostics/CodeAnalysis/NotNullIfNotNullAttribute.cs
+++
@@ -1,9 +0,0 @@
-#if NETSTANDARD2_0
-namespace System.Diagnostics.CodeAnalysis;
-
-[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
-internal sealed class NotNullIfNotNullAttribute(string parameterName) : Attribute
-{
-    public string ParameterName { get; } = parameterName;
-}
-#endif
--- src/HotAvalonia/Compat/System/Diagnostics/CodeAnalysis/NotNullWhenAttribute.cs
+++
@@ -1,8 +0,0 @@
-#if NETSTANDARD2_0
-namespace System.Diagnostics.CodeAnalysis;
-
-internal sealed class NotNullWhenAttribute(bool returnValue) : Attribute
-{
-    public bool ReturnValue { get; } = returnValue;
-}
-#endif
